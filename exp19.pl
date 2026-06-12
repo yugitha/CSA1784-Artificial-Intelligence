501b25b08e5dd988c6659b6de0cdfb36d76cd192
yugitha <yugithab5@gmail.com>
2026-06-12 09:00:21 +0530

    Add files via upload

---
+++ exp19.pl
@@ -0,0 +1,7 @@
+% student(StudentName, TeacherName, Subject, SubjectCode)
+
+student(arun, raj, ai, cs301).
+student(priya, kumar, dbms, cs302).
+student(vijay, raj, ai, cs301).
+student(divya, lakshmi, os, cs303).
+student(karthik, kumar, dbms, cs302).
