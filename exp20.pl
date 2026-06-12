91c8da0ce70f00ebaff1d497d5164c42a0f0b699
yugitha <yugithab5@gmail.com>
2026-06-12 09:03:32 +0530

    Add files via upload

---
+++ exp20.pl
@@ -0,0 +1,10 @@
+% planet(Name, DistanceFromSun, Type)
+
+planet(mercury, 58, terrestrial).
+planet(venus, 108, terrestrial).
+planet(earth, 150, terrestrial).
+planet(mars, 228, terrestrial).
+planet(jupiter, 778, gas_giant).
+planet(saturn, 1433, gas_giant).
+planet(uranus, 2872, ice_giant).
+planet(neptune, 4495, ice_giant).
