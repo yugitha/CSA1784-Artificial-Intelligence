790b19f98daacb3f35902af7eddf679acdfdbc68
yugitha <yugithab5@gmail.com>
2026-06-16 12:37:59 +0530

    Add files via upload

---
+++ exp25.pl
@@ -0,0 +1,8 @@
+% Goal State
+
+can_get_banana :-
+    write('Monkey moves to the box'), nl,
+    write('Monkey pushes the box under the banana'), nl,
+    write('Monkey climbs onto the box'), nl,
+    write('Monkey grabs the banana'), nl,
+    write('Goal Achieved: Monkey got the banana!'), nl.
